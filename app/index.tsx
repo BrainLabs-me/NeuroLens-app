066d669cf3f35011aea51662d49efb4e2bad486b
unknown <nikcevici13@gmail.com>
2024-11-26 19:26:22 +0100

    nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,15 @@
-import { View } from "react-native";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native";
 
 export default function IndexPage() {
-  return <View></View>;
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Hello! I am starting from Home</Text>
+      <StatusBar style="auto" />
+      <Link href="/(tabs)" style={{ color: "blue" }}>
+        Go To Profile
+      </Link>
+    </View>
+  );
 }
