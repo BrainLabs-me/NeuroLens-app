064badade6521032374d0ffddde9c84ae8fde793
unknown <nikcevici13@gmail.com>
2025-01-22 10:46:34 +0100

    small change

--- app/(app)/(protected)/(settings)/privacy-policy.tsx
+++ app/(app)/(protected)/(settings)/privacy-policy.tsx
@@ -1,282 +1,178 @@
-// import { ArrowLeft } from "iconsax-react-native";
-// import {
-//   View,
-//   TouchableOpacity,
-//   ScrollView,
-//   Text,
-//   StyleSheet,
-// } from "react-native";
-
-// import { Image } from "expo-image";
-// import { Image as RnImage } from "react-native";
-// import { LinearGradient } from "expo-linear-gradient";
-// import React, { useState } from "react";
-
-// import { router } from "expo-router";
-// import { useTranslation } from "react-i18next";
-
-// export default function Page() {
-//   const { t, i18n } = useTranslation();
-
-//   // State for toggling language
-//   const [language, setLanguage] = useState(i18n.language);
-
-//   const toggleLanguage = () => {
-//     const newLanguage = language === "en" ? "me" : "en";
-//     setLanguage(newLanguage);
-//     i18n.changeLanguage(newLanguage);
-//   };
-
-//   return (
-//     <>
-//       <View style={styles.background}></View>
-//       <View style={styles.bannerContainer}>
-//         <Image
-//           contentPosition={"bottom"}
-//           contentFit="cover"
-//           source={require("@/assets/images/profile_banner-2.png")}
-//           style={styles.banner}
-//         ></Image>
-//         <LinearGradient
-//           colors={["transparent", "transparent", "#00000F", "#00000F"]}
-//           style={styles.gradient}
-//         />
-//       </View>
-//       <RnImage
-//         style={styles.bgImage}
-//         source={require("@/assets/images/bg-3.png")}
-//       ></RnImage>
-//       <Image
-//         contentFit="contain"
-//         source={require("@/assets/images/logo_horizontal.png")}
-//         style={styles.logo}
-//       ></Image>
-//       <TouchableOpacity onPress={() => router.back()} style={styles.backButton}>
-//         <ArrowLeft color="white" size={28}></ArrowLeft>
-//       </TouchableOpacity>
-
-//       {/* Button to toggle language */}
-//       <TouchableOpacity onPress={toggleLanguage} style={styles.languageButton}>
-//         <Text style={styles.languageButtonText}>
-//           {language === "en" ? "Switch to Montenegrin" : "Switch to English"}
-//         </Text>
-//       </TouchableOpacity>
-
-//       {/* ScrollView for Privacy Policy */}
-//       <ScrollView contentContainerStyle={styles.contentContainer}>
-//         <Text style={styles.header}>{t("privacy-policy.title")}</Text>
-//         <Text style={styles.sectionHeader}>
-//           {t("privacy-policy.effective-date.title")}
-//         </Text>
-//         <Text style={styles.paragraph}>
-//           {t("privacy-policy.effective-date.text")}
-//         </Text>
-//         <Text style={styles.sectionHeader}>
-//           {t("privacy-policy.info-we-collect.title")}
-//         </Text>
-//         <Text style={styles.paragraph}>
-//           {t("privacy-policy.info-we-collect.text")}
-//         </Text>
-//         <Text style={styles.sectionHeader}>
-//           {t("privacy-policy.how-we-use.title")}
-//         </Text>
-//         <Text style={styles.paragraph}>
-//           {t("privacy-policy.how-we-use.text")}
-//         </Text>
-//         <Text style={styles.sectionHeader}>
-//           {t("privacy-policy.third-party.title")}
-//         </Text>
-//         <Text style={styles.paragraph}>
-//           {t("privacy-policy.third-party.text")}
-//         </Text>
-//         <Text style={styles.sectionHeader}>
-//           {t("privacy-policy.contact-us.title")}
-//         </Text>
-//         <Text style={styles.paragraph}>
-//           {t("privacy-policy.contact-us.text")}
-//         </Text>
-//       </ScrollView>
-//     </>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   background: {
-//     backgroundColor: "#00000F",
-//     position: "absolute",
-//     width: "100%",
-//     height: "100%",
-//   },
-//   bannerContainer: {
-//     overflow: "hidden",
-//     height: 200,
-//   },
-//   banner: {
-//     width: "100%",
-//     height: 230,
-//     position: "absolute",
-//   },
-//   gradient: {
-//     width: "100%",
-//     height: 300,
-//   },
-//   bgImage: {
-//     width: "100%",
-//     position: "absolute",
-//     height: "100%",
-//   },
-//   logo: {
-//     width: 170,
-//     height: 50,
-//     position: "absolute",
-//     top: 60,
-//     left: 30,
-//   },
-//   backButton: {
-//     alignItems: "center",
-//     justifyContent: "center",
-//     position: "absolute",
-//     top: 60,
-//     right: 30,
-//   },
-//   languageButton: {
-//     position: "absolute",
-//     top: 120,
-//     right: 30,
-//     backgroundColor: "#ffffff20",
-//     paddingVertical: 8,
-//     paddingHorizontal: 12,
-//     borderRadius: 8,
-//   },
-//   languageButtonText: {
-//     color: "white",
-//     fontSize: 14,
-//     fontWeight: "bold",
-//   },
-//   contentContainer: {
-//     padding: 20,
-//     paddingTop: 20,
-//   },
-//   header: {
-//     fontSize: 24,
-//     fontWeight: "bold",
-//     color: "white",
-//     marginBottom: 15,
-//   },
-//   sectionHeader: {
-//     fontSize: 18,
-//     fontWeight: "bold",
-//     color: "white",
-//     marginTop: 20,
-//     marginBottom: 10,
-//   },
-//   paragraph: {
-//     fontSize: 16,
-//     color: "white",
-//     lineHeight: 24,
-//   },
-// });
-
-import Card from "@/components/ui/card";
-import H1, { P } from "@/components/ui/text";
+import { ArrowLeft } from "iconsax-react-native";
 import {
-  ArrowRight2,
-  BrushBig,
-  Logout,
-  Notification1,
-  SecuritySafe,
-  SecurityUser,
-} from "iconsax-react-native";
-import { View, Text, TouchableOpacity } from "react-native";
-import { Image, ImageBackground } from "expo-image";
-import { Image as RnImage } from "react-native";
+  View,
+  TouchableOpacity,
+  ScrollView,
+  Text,
+  StyleSheet,
+} from "react-native";
 
+import { Image } from "expo-image";
+import { Image as RnImage } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import React, { useEffect, useState } from "react";
-import Modal from "@/components/ui/modal";
+import React, { useState } from "react";
+
 import { router } from "expo-router";
-import { useUser } from "@/context/userContext";
 import { useTranslation } from "react-i18next";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import Header from "@/components/navigation/header";
-import { Switch } from "react-native-switch";
+
 export default function Page() {
   const { t, i18n } = useTranslation();
-  const text = t("welcome");
 
-  const currentLanguage = i18n.language;
+  // State for toggling language
+  const [language, setLanguage] = useState(i18n.language);
 
-  const changeLanguage = async (lang: string) => {
-    await AsyncStorage.setItem("language", lang);
-    i18n.changeLanguage(lang);
+  const toggleLanguage = () => {
+    const newLanguage = language === "en" ? "me" : "en";
+    setLanguage(newLanguage);
+    i18n.changeLanguage(newLanguage);
   };
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
-  const { user, logout } = useUser();
+
   return (
     <>
-      <View className="bg-[#00000F] absolute w-full h-full"></View>
-      <View className="overflow-hidden h-[200px]">
+      <View style={styles.background}></View>
+      <View style={styles.bannerContainer}>
         <Image
           contentPosition={"bottom"}
           contentFit="cover"
           source={require("@/assets/images/profile_banner-2.png")}
-          style={{
-            width: "100%",
-            height: 230,
-            position: "absolute",
-          }}
+          style={styles.banner}
         ></Image>
         <LinearGradient
           colors={["transparent", "transparent", "#00000F", "#00000F"]}
-          style={{
-            width: "100%",
-            height: 300,
-          }}
+          style={styles.gradient}
         />
       </View>
       <RnImage
-        className="w-full absolute  h-full"
+        style={styles.bgImage}
         source={require("@/assets/images/bg-3.png")}
       ></RnImage>
-      <Header></Header>
-      <View className="flex-1 gap-4 items-center">
-        <View className="w-full gap-5">
-          <View className="flex flex-col gap-2">
-            <H1>Security</H1>
-            <P>Manage your account security</P>
-          </View>
-          <Card className="text-center justify-center items-center py-12 gap-4 mt-5 rounded-none">
-            <TouchableOpacity
-              className="flex-row items-center justify-between w-full"
-              onPress={() => router.push("/(app)/forgot-password")}
-            >
-              <View className="flex-row items-center gap-3">
-                <SecuritySafe color="white" size={28}></SecuritySafe>
-                <P className="text-[16px]">{t("security.change-password")}</P>
-              </View>
-              <ArrowRight2 color="white" size={28}></ArrowRight2>
-            </TouchableOpacity>
-            <View className="h-[1px] bg-border w-[88%] "></View>
-            <TouchableOpacity
-              className="flex-row items-center justify-between w-full mt-6"
-              onPress={() =>
-                // router.push("/(app)/(protected)/(settings)/privacy-policy")
-                "DELETE ACCOUNT REQ"
-              }
-            >
-              <View className="flex-row items-center gap-3">
-                <SecurityUser color="red" size={28}></SecurityUser>
-                <P className="text-[16px] text-red-600">
-                  {t("security.delete-account")}
-                </P>
-              </View>
-              <ArrowRight2 color="white" size={28}></ArrowRight2>
-            </TouchableOpacity>
-            <View className="h-[1px] bg-border w-[88%] "></View>
-          </Card>
-        </View>
-      </View>
+      <Image
+        contentFit="contain"
+        source={require("@/assets/images/logo_horizontal.png")}
+        style={styles.logo}
+      ></Image>
+      <TouchableOpacity onPress={() => router.back()} style={styles.backButton}>
+        <ArrowLeft color="white" size={28}></ArrowLeft>
+      </TouchableOpacity>
+
+      {/* Button to toggle language */}
+      <TouchableOpacity onPress={toggleLanguage} style={styles.languageButton}>
+        <Text style={styles.languageButtonText}>
+          {language === "en" ? "Switch to Montenegrin" : "Switch to English"}
+        </Text>
+      </TouchableOpacity>
+
+      {/* ScrollView for Privacy Policy */}
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <Text style={styles.header}>{t("privacy-policy.title")}</Text>
+        <Text style={styles.sectionHeader}>
+          {t("privacy-policy.effective-date.title")}
+        </Text>
+        <Text style={styles.paragraph}>
+          {t("privacy-policy.effective-date.text")}
+        </Text>
+        <Text style={styles.sectionHeader}>
+          {t("privacy-policy.info-we-collect.title")}
+        </Text>
+        <Text style={styles.paragraph}>
+          {t("privacy-policy.info-we-collect.text")}
+        </Text>
+        <Text style={styles.sectionHeader}>
+          {t("privacy-policy.how-we-use.title")}
+        </Text>
+        <Text style={styles.paragraph}>
+          {t("privacy-policy.how-we-use.text")}
+        </Text>
+        <Text style={styles.sectionHeader}>
+          {t("privacy-policy.third-party.title")}
+        </Text>
+        <Text style={styles.paragraph}>
+          {t("privacy-policy.third-party.text")}
+        </Text>
+        <Text style={styles.sectionHeader}>
+          {t("privacy-policy.contact-us.title")}
+        </Text>
+        <Text style={styles.paragraph}>
+          {t("privacy-policy.contact-us.text")}
+        </Text>
+      </ScrollView>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#00000F",
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+  bannerContainer: {
+    overflow: "hidden",
+    height: 200,
+  },
+  banner: {
+    width: "100%",
+    height: 230,
+    position: "absolute",
+  },
+  gradient: {
+    width: "100%",
+    height: 300,
+  },
+  bgImage: {
+    width: "100%",
+    position: "absolute",
+    height: "100%",
+  },
+  logo: {
+    width: 170,
+    height: 50,
+    position: "absolute",
+    top: 60,
+    left: 30,
+  },
+  backButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    top: 60,
+    right: 30,
+  },
+  languageButton: {
+    position: "absolute",
+    top: 120,
+    right: 30,
+    backgroundColor: "#ffffff20",
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    borderRadius: 8,
+  },
+  languageButtonText: {
+    color: "white",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  contentContainer: {
+    padding: 20,
+    paddingTop: 20,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    marginBottom: 15,
+  },
+  sectionHeader: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  paragraph: {
+    fontSize: 16,
+    color: "white",
+    lineHeight: 24,
+  },
+});
--- app/(app)/(protected)/(settings)/security.tsx
+++ app/(app)/(protected)/(settings)/security.tsx
@@ -9,7 +9,6 @@ import {
   SecurityUser,
 } from "iconsax-react-native";
 import { View, Text, TouchableOpacity } from "react-native";
-import Gravatar from "@/components/ui/gravatar";
 import { Image, ImageBackground } from "expo-image";
 import { Image as RnImage } from "react-native";
 
@@ -62,75 +61,40 @@ export default function Page() {
         source={require("@/assets/images/bg-3.png")}
       ></RnImage>
       <Header></Header>
-      <View className="flex-1 px-5 gap-2  items-center  translate-y-[-60px]">
+      <View className="flex-1 gap-4 items-center">
         <View className="w-full gap-5">
-          <Card className="text-center justify-center items-center mt-5 rounded-none">
-            <TouchableOpacity className="flex-row items-center justify-between w-full">
+          <View className="flex flex-col gap-2">
+            <H1>Security</H1>
+            <P>Manage your account security</P>
+          </View>
+          <Card className="text-center justify-center items-center py-12 gap-4 mt-5 rounded-none">
+            <TouchableOpacity
+              className="flex-row items-center justify-between w-full"
+              onPress={() => router.push("/(app)/forgot-password")}
+            >
               <View className="flex-row items-center gap-3">
                 <SecuritySafe color="white" size={28}></SecuritySafe>
-                <P className="text-[16px]">{t("profile.security")}</P>
+                <P className="text-[16px]">{t("security.change-password")}</P>
               </View>
               <ArrowRight2 color="white" size={28}></ArrowRight2>
             </TouchableOpacity>
             <View className="h-[1px] bg-border w-[88%] "></View>
             <TouchableOpacity
-              className="flex-row items-center justify-between w-full"
+              className="flex-row items-center justify-between w-full mt-6"
               onPress={() =>
-                router.push("/(app)/(protected)/(settings)/privacy-policy")
+                // router.push("/(app)/(protected)/(settings)/privacy-policy")
+                "DELETE ACCOUNT REQ"
               }
             >
               <View className="flex-row items-center gap-3">
-                <SecurityUser color="white" size={28}></SecurityUser>
-                <P className="text-[16px]">{t("profile.privacy-policy")}</P>
+                <SecurityUser color="red" size={28}></SecurityUser>
+                <P className="text-[16px] text-red-600">
+                  {t("security.delete-account")}
+                </P>
               </View>
               <ArrowRight2 color="white" size={28}></ArrowRight2>
             </TouchableOpacity>
             <View className="h-[1px] bg-border w-[88%] "></View>
-            <TouchableOpacity
-              onPress={() =>
-                router.push("/(app)/(protected)/(settings)/language")
-              }
-              className="flex-row items-center justify-between w-full"
-            >
-              <View className="flex-row items-center gap-3">
-                <BrushBig color="white" size={28}></BrushBig>
-                <P className="text-[16px]">{t("profile.language")}</P>
-              </View>
-              <ArrowRight2 color="white" size={28}></ArrowRight2>
-            </TouchableOpacity>
-            <View className="h-[1px] bg-border w-[88%] "></View>
-            <TouchableOpacity className="flex-row items-center justify-between w-full">
-              <View className="flex-row items-center gap-3">
-                <Notification1 color="white" size={28}></Notification1>
-                <P className="text-[16px]">{t("profile.notifications")}</P>
-              </View>
-              <Switch
-                value={isEnabled}
-                onValueChange={(val) => toggleSwitch()}
-                backgroundActive={"#6747ED"}
-                backgroundInactive={"gray"}
-                renderActiveText={false}
-                circleBorderActiveColor="#6747ED"
-                circleBorderInactiveColor="gray"
-                renderInActiveText={false}
-              />
-            </TouchableOpacity>
-          </Card>
-          <Card className="py-3">
-            <Modal
-              heading={"Log out?"}
-              onPress={() => logout()}
-              description={`${t("profile.log-out-action")} ${user?.name}`}
-              trigger={
-                <View className="flex-row items-center justify-between">
-                  <View className="flex-row items-center gap-3">
-                    <Logout color="white" size={28}></Logout>
-                    <P className="text-[16px]">{t("profile.log-out")}</P>
-                  </View>
-                  <ArrowRight2 color="white" size={28}></ArrowRight2>
-                </View>
-              }
-            ></Modal>
           </Card>
         </View>
       </View>
